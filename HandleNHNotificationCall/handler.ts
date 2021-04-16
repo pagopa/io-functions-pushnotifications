64670a38e348ef09c7660c681c5a702e8b7d9b68
gquadrati <greta.quadrati@pagopa.it>
2021-04-16 10:00:52 +0200

    minor changes

--- HandleNHNotificationCall/handler.ts
+++ HandleNHNotificationCall/handler.ts
@@ -1,5 +1,7 @@
 import { Context } from "@azure/functions";
 import * as df from "durable-functions";
+import { DurableOrchestrationClient } from "durable-functions/lib/src/durableorchestrationclient";
+
 import * as t from "io-ts";
 import { toString } from "fp-ts/lib/function";
 
@@ -23,23 +25,22 @@ export const NotificationMessage = t.union([
 
 export type NotificationHubMessage = t.TypeOf<typeof NotificationMessage>;
 
-/**
- * Invoke Orchestrator to manage Notification Hub Service call with data provided by an enqued message
- */
-export const getHandler = () => async (
+const startOrchestrator = async (
+  notificationHubMessage: NotificationHubMessage,
   context: Context,
-  notificationHubMessage: NotificationHubMessage
+  client: DurableOrchestrationClient
 ): Promise<string> => {
-  const client = df.getClient(context);
-
-  // eslint-disable-next-line default-case
   switch (notificationHubMessage.kind) {
     case DeleteKind.DeleteInstallation:
-      return client.startNew(DeleteInstallationOrchestratorName, undefined, {
-        message: notificationHubMessage
-      });
+      return await client.startNew(
+        DeleteInstallationOrchestratorName,
+        undefined,
+        {
+          message: notificationHubMessage
+        }
+      );
     case CreateOrUpdateKind.CreateOrUpdateInstallation:
-      return client.startNew(
+      return await client.startNew(
         CreateOrUpdateInstallationOrchestrator,
         undefined,
         {
@@ -47,7 +48,7 @@ export const getHandler = () => async (
         }
       );
     case NotifyKind.Notify:
-      return client.startNew(NotifyMessageOrchestratorName, undefined, {
+      return await client.startNew(NotifyMessageOrchestratorName, undefined, {
         message: notificationHubMessage
       });
     default:
@@ -60,4 +61,16 @@ export const getHandler = () => async (
         `Unknown message kind, message: ${toString(notificationHubMessage)}`
       );
   }
+};
+
+/**
+ * Invoke Orchestrator to manage Notification Hub Service call with data provided by an enqued message
+ */
+export const getHandler = () => async (
+  context: Context,
+  notificationHubMessage: NotificationHubMessage
+): Promise<string> => {
+  const client = df.getClient(context);
+
+  return startOrchestrator(notificationHubMessage, context, client);
 };
