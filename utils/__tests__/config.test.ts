a6c10e4fab1ce63afc0b9a042ac39fdaf1e92b36
Emanuele De Cupis <ema.decu@gmail.com>
2021-04-27 18:16:38 +0200

    [#IP-146] [#IP-146] compute AZURE_NOTIFICATION_HUB_PARTITIONS

--- utils/__tests__/config.test.ts
+++ utils/__tests__/config.test.ts
@@ -1,9 +1,10 @@
 import { isRight } from "fp-ts/lib/Either";
 import { IConfig } from "../config";
-import { envConfig } from "../../__mocks__/env-config.mock";
+import { envConfig } from "../../__mocks__/env.mock";
 import { NonEmptyString } from "italia-ts-commons/lib/strings";
+import { readableReport } from "italia-ts-commons/lib/reporters";
 
-const aConfig = envConfig;
+const aConfig = { ...envConfig, isProduction: false };
 
 beforeEach(() => {
   jest.clearAllMocks();
@@ -20,8 +21,9 @@ describe("IConfig", () => {
     var decoded = IConfig.decode({
       ...aConfig,
       NH_PARTITION_FEATURE_FLAG: ff
-    });
-    expect(isRight(decoded)).toBe(true);
+    }).getOrElseL(e => fail(`Cannot decode config, ${readableReport(e)}`));
+
+    expect(typeof decoded).toBe("object");
   });
 
   it("should throw error with wrong FF inputs", () => {
@@ -33,95 +35,13 @@ describe("IConfig", () => {
     expect(isRight(decoded)).toBe(false);
   });
 
-  it("should throw error with wrong array of NH partitions - wrong object", () => {
-    var configDecodedWrong = IConfig.decode({
+  it("should not override computed value for AZURE_NOTIFICATION_HUB_PARTITIONS", () => {
+    var config = IConfig.decode({
       ...aConfig,
-      AZURE_NOTIFICATION_HUB_PARTITIONS:
-        '[{ "partitionRegex": "^[0-3]", "name": "name", }]'
-    });
+      AZURE_NOTIFICATION_HUB_PARTITIONS: "any value"
+    }).getOrElseL(e => fail(`Cannot decode config, ${readableReport(e)}`));
 
-    expect(isRight(configDecodedWrong)).toBe(false);
-  });
-
-  it("should throw error with wrong array of NH partitions - not an array", () => {
-    var configDecodedWrong = IConfig.decode({
-      ...aConfig,
-      AZURE_NOTIFICATION_HUB_PARTITIONS: JSON.stringify({
-        partitionRegex: "^[c-f]" as NonEmptyString,
-        name: "io-notification-hub-mock" as NonEmptyString,
-        namespace: "io-p-ntfns-sandbox" as NonEmptyString,
-        sharedAccessKey: "anAccessKey" as NonEmptyString
-      })
-    });
-
-    expect(isRight(configDecodedWrong)).toBe(false);
-  });
-
-  it("should throw error with wrong array of NH partitions - at least one regex not starting from first character", () => {
-    var configDecodedWrong = IConfig.decode({
-      ...aConfig,
-      AZURE_NOTIFICATION_HUB_PARTITIONS: JSON.stringify([
-        {
-          partitionRegex: "^[0-3]" as NonEmptyString,
-          name: "io-notification-hub-mock" as NonEmptyString,
-          namespace: "io-p-ntfns-sandbox" as NonEmptyString,
-          sharedAccessKey: "anAccessKey" as NonEmptyString
-        },
-        {
-          partitionRegex: "[4-7]" as NonEmptyString,
-          name: "io-notification-hub-mock" as NonEmptyString,
-          namespace: "io-p-ntfns-sandbox" as NonEmptyString,
-          sharedAccessKey: "anAccessKey" as NonEmptyString
-        },
-        {
-          partitionRegex: "^[8-b]" as NonEmptyString,
-          name: "io-notification-hub-mock" as NonEmptyString,
-          namespace: "io-p-ntfns-sandbox" as NonEmptyString,
-          sharedAccessKey: "anAccessKey" as NonEmptyString
-        },
-        {
-          partitionRegex: "^[c-f]" as NonEmptyString,
-          name: "io-notification-hub-mock" as NonEmptyString,
-          namespace: "io-p-ntfns-sandbox" as NonEmptyString,
-          sharedAccessKey: "anAccessKey" as NonEmptyString
-        }
-      ])
-    });
-
-    expect(isRight(configDecodedWrong)).toBe(false);
-  });
-
-  it("should throw error with wrong array of NH partitions - overlapping regex", () => {
-    var configDecodedWrong = IConfig.decode({
-      ...aConfig,
-      AZURE_NOTIFICATION_HUB_PARTITIONS: JSON.stringify([
-        {
-          partitionRegex: "^[0-8]" as NonEmptyString,
-          name: "io-notification-hub-mock" as NonEmptyString,
-          namespace: "io-p-ntfns-sandbox" as NonEmptyString,
-          sharedAccessKey: "anAccessKey" as NonEmptyString
-        },
-        {
-          partitionRegex: "^[4-7]" as NonEmptyString,
-          name: "io-notification-hub-mock" as NonEmptyString,
-          namespace: "io-p-ntfns-sandbox" as NonEmptyString,
-          sharedAccessKey: "anAccessKey" as NonEmptyString
-        },
-        {
-          partitionRegex: "^[8-b]" as NonEmptyString,
-          name: "io-notification-hub-mock" as NonEmptyString,
-          namespace: "io-p-ntfns-sandbox" as NonEmptyString,
-          sharedAccessKey: "anAccessKey" as NonEmptyString
-        },
-        {
-          partitionRegex: "^[c-f]" as NonEmptyString,
-          name: "io-notification-hub-mock" as NonEmptyString,
-          namespace: "io-p-ntfns-sandbox" as NonEmptyString,
-          sharedAccessKey: "anAccessKey" as NonEmptyString
-        }
-      ])
-    });
-
-    expect(isRight(configDecodedWrong)).toBe(false);
+    expect(config.AZURE_NOTIFICATION_HUB_PARTITIONS).toEqual(expect.any(Array));
+    expect(config.AZURE_NOTIFICATION_HUB_PARTITIONS).not.toBe("any value");
   });
 });
--- utils/config.ts
+++ utils/config.ts
@@ -5,6 +5,7 @@
  * The configuration is evaluate eagerly at the first access to the module. The module exposes convenient methods to access such value.
  */
 
+import { None } from "fp-ts/lib/Option";
 import * as t from "io-ts";
 import { IntegerFromString } from "italia-ts-commons/lib/numbers";
 import { readableReport } from "italia-ts-commons/lib/reporters";
@@ -12,7 +13,9 @@ import { NonEmptyString } from "italia-ts-commons/lib/strings";
 import { withDefault } from "italia-ts-commons/lib/types";
 import {
   DisjoitedNotificationHubPartitionArray,
-  jsonFromString
+  jsonFromString,
+  NotificationHubPartition,
+  RegExpFromString
 } from "./types";
 
 export type NHPartitionFeatureFlag = t.TypeOf<typeof NHPartitionFeatureFlag>;
@@ -24,8 +27,9 @@ export const NHPartitionFeatureFlag = t.keyof({
 });
 
 // global app configuration
-export type IConfig = t.TypeOf<typeof IConfig>;
-export const IConfig = t.intersection([
+//const UnknownToEnvWithScalarValues = {} as t.Mixed; // {...process.env, NH1_NAME.....}
+export type BaseConfig = t.TypeOf<typeof BaseConfig>;
+const BaseConfig = t.intersection([
   t.interface({
     APPINSIGHTS_INSTRUMENTATIONKEY: NonEmptyString,
     // the internal function runtime has MaxTelemetryItem per second set to 20 by default
@@ -44,9 +48,21 @@ export const IConfig = t.intersection([
     AZURE_NH_ENDPOINT: NonEmptyString,
     AZURE_NH_HUB_NAME: NonEmptyString,
 
-    AZURE_NOTIFICATION_HUB_PARTITIONS: jsonFromString.pipe(
-      DisjoitedNotificationHubPartitionArray
-    )
+    NH1_ENDPOINT: NonEmptyString,
+    NH1_NAME: NonEmptyString,
+    NH1_PARTITION_REGEX: RegExpFromString,
+
+    NH2_ENDPOINT: NonEmptyString,
+    NH2_NAME: NonEmptyString,
+    NH2_PARTITION_REGEX: RegExpFromString,
+
+    NH3_ENDPOINT: NonEmptyString,
+    NH3_NAME: NonEmptyString,
+    NH3_PARTITION_REGEX: RegExpFromString,
+
+    NH4_ENDPOINT: NonEmptyString,
+    NH4_NAME: NonEmptyString,
+    NH4_PARTITION_REGEX: RegExpFromString
   }),
 
   t.interface({
@@ -57,6 +73,94 @@ export const IConfig = t.intersection([
   }),
   t.partial({ APPINSIGHTS_DISABLE: t.string })
 ]);
+
+type A = Exclude<
+  BaseConfig,
+  | "NH1_ENDPOINT"
+  | "NH1_NAME"
+  | "NH1_PARTITION_REGEX"
+  | "NH2_ENDPOINT"
+  | "NH2_NAME"
+  | "NH2_PARTITION_REGEX"
+  | "NH3_ENDPOINT"
+  | "NH3_NAME"
+  | "NH3_PARTITION_REGEX"
+  | "NH4_ENDPOINT"
+  | "NH4_NAME"
+  | "NH4_PARTITION_REGEX"
+> & {
+  readonly AZURE_NOTIFICATION_HUB_PARTITIONS: DisjoitedNotificationHubPartitionArray;
+};
+
+const NhConfigToBase = new t.Type<A, BaseConfig, BaseConfig>(
+  "NhConfigToBase",
+  (v: unknown): v is A =>
+    BaseConfig.is(v) && "AZURE_NOTIFICATION_HUB_PARTITIONS" in v,
+  (v, _c): t.Validation<A> => {
+    const {
+      NH1_ENDPOINT,
+      NH1_NAME,
+      NH1_PARTITION_REGEX,
+      NH2_ENDPOINT,
+      NH2_NAME,
+      NH2_PARTITION_REGEX,
+      NH3_ENDPOINT,
+      NH3_NAME,
+      NH3_PARTITION_REGEX,
+      NH4_ENDPOINT,
+      NH4_NAME,
+      NH4_PARTITION_REGEX,
+      ...rest
+    } = v;
+    const baseConfig = rest as BaseConfig;
+    const nhPartitions = [
+      {
+        endpoint: NH1_ENDPOINT,
+        name: NH1_NAME,
+        partitionRegex: NH1_PARTITION_REGEX
+      },
+      {
+        endpoint: NH2_ENDPOINT,
+        name: NH2_NAME,
+        partitionRegex: NH2_PARTITION_REGEX
+      },
+      {
+        endpoint: NH3_ENDPOINT,
+        name: NH3_NAME,
+        partitionRegex: NH3_PARTITION_REGEX
+      },
+      {
+        endpoint: NH4_ENDPOINT,
+        name: NH4_NAME,
+        partitionRegex: NH4_PARTITION_REGEX
+      }
+    ];
+    return DisjoitedNotificationHubPartitionArray.decode(nhPartitions).map(
+      partitions => ({
+        ...baseConfig,
+        AZURE_NOTIFICATION_HUB_PARTITIONS: partitions
+      })
+    );
+  },
+  (v: A): BaseConfig => {
+    const { AZURE_NOTIFICATION_HUB_PARTITIONS, ...rest } = v;
+    return {
+      ...rest,
+      ...AZURE_NOTIFICATION_HUB_PARTITIONS.reduce(
+        (p, e, i) => ({
+          ...p,
+          [`NH${i}_ENDPOINT`]: e.endpoint,
+          [`NH${i}_NAME`]: e.name,
+          [`NH${i}_PARTITION_REGEX`]: e.partitionRegex
+        }),
+        {}
+      )
+    };
+  }
+);
+
+export type IConfig = t.TypeOf<typeof IConfig>;
+export const IConfig = BaseConfig.pipe(NhConfigToBase);
 
 // No need to re-evaluate this object for each call
 const errorOrConfig: t.Validation<IConfig> = IConfig.decode({
