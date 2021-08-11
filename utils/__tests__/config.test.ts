86b16868dabe8c1acdbd44022e527a9494069e22
gquadrati <greta.quadrati@pagopa.it>
2021-08-11 16:49:41 +0200

    [#IP-342] update utils/__tests__/config.test.ts

--- utils/__tests__/config.test.ts
+++ utils/__tests__/config.test.ts
@@ -3,6 +3,9 @@ import { IConfig } from "../config";
 import { envConfig } from "../../__mocks__/env.mock";
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import { readableReport } from "@pagopa/ts-commons/lib/reporters";
+import { pipe } from "fp-ts/lib/function";
+
+import * as E from "fp-ts/lib/Either";
 
 const aConfig = { ...envConfig, isProduction: false };
 
@@ -18,10 +21,14 @@ describe("IConfig", () => {
     ${"beta"}
     ${"canary"}
   `("should deserialize config with $ff user subset", ({ ff }) => {
-    var decoded = IConfig.decode({
-      ...aConfig,
-      NH_PARTITION_FEATURE_FLAG: ff
-    }).getOrElseL(e => fail(`Cannot decode config, ${readableReport(e)}`));
+    var decoded = pipe(
+      {
+        ...aConfig,
+        NH_PARTITION_FEATURE_FLAG: ff
+      },
+      IConfig.decode,
+      E.getOrElseW(e => fail(`Cannot decode config, ${readableReport(e)}`))
+    );
 
     expect(typeof decoded).toBe("object");
   });
@@ -36,10 +43,14 @@ describe("IConfig", () => {
   });
 
   it("should not override computed value for AZURE_NOTIFICATION_HUB_PARTITIONS", () => {
-    var config = IConfig.decode({
-      ...aConfig,
-      AZURE_NOTIFICATION_HUB_PARTITIONS: "any value"
-    }).getOrElseL(e => fail(`Cannot decode config, ${readableReport(e)}`));
+    var config = pipe(
+      {
+        ...aConfig,
+        AZURE_NOTIFICATION_HUB_PARTITIONS: "any value"
+      },
+      IConfig.decode,
+      E.getOrElseW(e => fail(`Cannot decode config, ${readableReport(e)}`))
+    );
 
     expect(config.AZURE_NOTIFICATION_HUB_PARTITIONS).toEqual(expect.any(Array));
     expect(config.AZURE_NOTIFICATION_HUB_PARTITIONS).not.toBe("any value");
