179806a515c988d75a9af09cb4b80454f96b1a23
gquadrati <greta.quadrati@pagopa.it>
2021-08-11 16:49:40 +0200

    [#IP-342] update Info/handler.ts

--- Info/handler.ts
+++ Info/handler.ts
@@ -6,6 +6,8 @@ import {
   ResponseErrorInternal,
   ResponseSuccessJson
 } from "@pagopa/ts-commons/lib/responses";
+import { pipe } from "fp-ts/lib/function";
+import * as TE from "fp-ts/lib/TaskEither";
 import * as packageJson from "../package.json";
 import { checkApplicationHealth, HealthCheck } from "../utils/healthcheck";
 
@@ -23,16 +25,18 @@ export const InfoHandler = (
 ): InfoHandler => (): Promise<
   IResponseSuccessJson<IInfo> | IResponseErrorInternal
 > =>
-  healthCheck
-    .fold<IResponseSuccessJson<IInfo> | IResponseErrorInternal>(
+  pipe(
+    healthCheck,
+    TE.bimap(
       problems => ResponseErrorInternal(problems.join("\n\n")),
       _ =>
         ResponseSuccessJson({
           name: packageJson.name,
           version: packageJson.version
         })
-    )
-    .run();
+    ),
+    TE.toUnion
+  )();
 
 export const Info = (): express.RequestHandler => {
   const handler = InfoHandler(checkApplicationHealth());
