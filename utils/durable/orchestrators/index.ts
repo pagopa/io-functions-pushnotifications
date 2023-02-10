3d1b9fbd7669cc6f7ea7f4255b630ce21fcde79f
AndreaPagoPa <120489894+AndreaPagoPa@users.noreply.github.com>
2023-02-10 15:11:39 +0000

    fix for error catch with unknown type

--- utils/durable/orchestrators/index.ts
+++ utils/durable/orchestrators/index.ts
@@ -124,7 +124,7 @@ export const callableActivity = <
     } catch (e) {
       throw failureActivity(
         activityName,
-        e instanceof Error ? e.message : "undefined error"
+        e instanceof Error ? e.message : ActivityResultFailure.is(e) ? e.reason : "Undefined error"
       );
     }
     return pipe(
